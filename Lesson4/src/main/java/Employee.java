99d0f06ad932d6dda89e7187f4d8bf3e4d3a066d
Pavel Sokolov <116973832+mrsok888@users.noreply.github.com>
2023-09-07 20:04:49 +0300

    2

--- Lesson4/src/main/java/Employee.java
+++ Lesson4/src/main/java/Employee.java
@@ -1,4 +1,5 @@
 public class Employee {
+    //1
     private String fullName;
     private String position;
     private String email;
@@ -22,10 +23,23 @@ public class Employee {
         System.out.println("Телефон: " + phoneNumber);
         System.out.println("Зарплата: " + salary);
         System.out.println("Возраст: " + age);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        Employee employee = new Employee("Петр Соклоков", "SDET", "PSESoklokov@sebr.ru", "89667778918", 350000, 39);
+        //1
+        Employee employee = new Employee("Петр Соклоков", "SDET", "PSESoklokov@sebr.ru", "+79667778918", 350000, 39);
         employee.printInfo();
+        //2
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Иван Иванович Иванов", "Инженер", "ivan@example.com", "+123456789", 300000, 35);
+        employees[1] = new Employee("Петр Петрович Петров", "Программист", "petr@example.com", "+987654321", 350000, 28);
+        employees[2] = new Employee("Анна Сидоровна Сидорова", "Дизайнер", "anna@example.com", "+111222333", 150000, 30);
+        employees[3] = new Employee("Сергей Сергеевич Сергеев", "Менеджер", "sergey@example.com", "+777888999", 200000, 32);
+        employees[4] = new Employee("Ольга Олеговна Олегова", "Бухгалтер", "olga@example.com", "+444555666", 100000, 29);
+
+        for (int i = 0; i < employees.length; ++i) {
+            employees[i].printInfo();
+        }
     }
 }
